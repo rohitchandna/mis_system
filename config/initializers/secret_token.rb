8f2dec4283877336180583d2eb3fef6bd4b9fd9e
rohitchandna <rohit.chandna@greenbackforex.com>
2014-05-29 10:59:58 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MisSystem::Application.config.secret_token = '42576994072008c5f45b634bca27dc72f6b843a45abde62c5c6158298ca80c3d02f8b32b500e2f688c4ab93e53e18df5c1a9d14053a57b6b3054f31d8847a86f'
